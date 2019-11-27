45d0a1852107d5b9f7829b30b1a5451ff697593a
Lippo <filippo.nardini@studio.unibo.it>
2019-11-27 13:23:08 +0000

    Checkpoint

---
+++ thesis-parts/auto/chapter-one.el
@@ -0,0 +1,15 @@
+(TeX-add-style-hook
+ "chapter-one"
+ (lambda ()
+   (LaTeX-add-labels
+    "fig:streetscene"
+    "sec:aggregate-computing"
+    "fig:device-centric"
+    "fig:aggregate-computing"
+    "fig:abstraction-layers"
+    "fig:op-g"
+    "subsec:Architettura di Protelis"
+    "fig:architettura-protelis"
+    "fig:estensione-protelis"))
+ :latex)
+
